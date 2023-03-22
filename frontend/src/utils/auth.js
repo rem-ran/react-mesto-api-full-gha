3f3d259d761a06088761121a9ff92f14d0d4a22d
Ruslan Timerbulatov <rem.ran@yandex.com>
2023-03-22 19:20:32 +0300

    change api urls to match domain

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,5 +1,5 @@
 // const BASE_URL = "https://auth.nomoreparties.co";
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://api.remran.nomoredomains.work";
 
 function getResponseData(res) {
   if (!res.ok) {
