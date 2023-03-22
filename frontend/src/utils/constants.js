5391f4cbfddf50f4c2515206839b9fb45f757193
Ruslan Timerbulatov <rem.ran@yandex.com>
2023-03-22 13:59:17 +0300

    create winston loggers

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,10 +1,8 @@
 //объект с нужными для работы с сервером данными
 export const apiConfig = {
-  // url: "https://mesto.nomoreparties.co/v1/cohort-54",
   url: 'http://localhost:3000',
   credentials: "include",
   headers: {
-    // authorization: '858bd672-ba6d-4b55-94fb-42daf32afd54',
     'Content-Type': 'application/json',
   },
 };
