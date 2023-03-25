39926fd5cc3bdcde1d623268e46ec3e50a191af8
Ruslan Timerbulatov <rem.ran@yandex.com>
2023-03-26 00:16:45 +0300

    delete localhosts urls

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,5 +1,4 @@
-// const BASE_URL = "https://api.remran.nomoredomains.work";
-const BASE_URL = 'http://localhost:3000';
+const BASE_URL = "https://api.remran.nomoredomains.work";
 
 function getResponseData(res) {
   if (!res.ok) {
